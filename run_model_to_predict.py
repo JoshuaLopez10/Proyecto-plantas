2a44da0e758c1b006b8ebbfa18d597c11cd5849a
JoshuaLopez10 <josh_alr10@hotmail.com>
2021-06-25 10:31:16 -0500

    Corrected

--- run_model_to_predict.py
+++ run_model_to_predict.py
@@ -1,19 +1,18 @@
-#%%
 from keras import models
+from PIL import Image
 from cv2 import imread
 from numpy import array
 
 new_model = models.load_model('plants_trainedNN.h5')
 
-img_dir = 'image_h4.png'
+img_dir = 'image_h3.png'
 image = array(imread(img_dir))
 image.resize(375, 250, 3)
-image = image.reshape(1, 375, 250, 3)
+image = image.reshape(1, 375, 250, 3).astype('float32') / 255
 
-#%%
 y = new_model.predict(image)
 
-if y == 0:
+if y < 0.5:
     print('healthy')
 else:
     print('not_healthy')
